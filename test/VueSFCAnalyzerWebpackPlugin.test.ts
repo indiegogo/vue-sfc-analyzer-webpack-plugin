fbf8a9f196e96c102446adae0719076b50df5448
Kengo Hamasaki <kengo.hamasaki@indiegogo.com>
2018-03-13 16:29:49 -0700

    Test webpack's results with some patterns of .vue

--- test/VueSFCAnalyzerWebpackPlugin.test.ts
+++ test/VueSFCAnalyzerWebpackPlugin.test.ts
@@ -1,6 +1,7 @@
 import Plugin, { VueSFCAnalyzerWebpackPluginOption } from "../src/VueSFCAnalyzerWebpackPlugin";
 import webpack from "webpack";
 import path from "path";
+import fs from "fs";
 
 describe("VueSFCAnalyzerWebpackPlugin", () => {
   describe("options", () => {
@@ -40,6 +41,19 @@ describe("VueSFCAnalyzerWebpackPlugin", () => {
 });
 
 describe("with Webpack", () => {
+  const defaultStatsPath = path.resolve(process.cwd(), "./vue_sfc_stats.json");
+  const customPath = path.resolve(process.cwd(), "./vue_sfc_stats_custom.json");
+  const fileExists = (path): boolean => fs.existsSync(path);
+
+  afterEach(() => {
+    // Clean up files which may exist
+    [defaultStatsPath, customPath].forEach((path) => {
+      if (fileExists(path)) {
+        fs.unlinkSync(path);
+      }
+    });
+  });
+
   jest.setTimeout(10000);
 
   const webpackOption = (plugin) => {
@@ -73,19 +87,104 @@ describe("with Webpack", () => {
     }
   };
 
-  it("records", (done) => {
-    const plugin = new Plugin({
-      statsFilename: false
+  describe("analysis result", () => {
+    let plugin;
+
+    const recordFor = (sfcFileName) => {
+      return plugin.records[`test/fixtures/${sfcFileName}`];
+    };
+
+    beforeEach(() => {
+      plugin = new Plugin({
+        statsFilename: false
+      });
     });
-    webpack(webpackOption(plugin)).run((err, stats) => {
-      const record = plugin.records["test/fixtures/TestComponent.vue"];
-      expect(record.template.size).toBeGreaterThan(0);
-      expect(record.template.source.length).toBeGreaterThan(0);
-      expect(record.script.size).toBeGreaterThan(0);
-      expect(record.script.source.length).toBeGreaterThan(0);
-      expect(record.style.size).toBeGreaterThan(0);
-      expect(record.style.sources.length).toBe(2);
-      done();
+
+    it("should record result of the basic component (TestComponent.vue)", (done) => {
+      webpack(webpackOption(plugin)).run((err, stats) => {
+        const record = recordFor("TestComponent.vue");
+        expect(record.template.size).toBeGreaterThan(0);
+        expect(record.template.source.length).toBeGreaterThan(0);
+        expect(record.script.size).toBeGreaterThan(0);
+        expect(record.script.source.length).toBeGreaterThan(0);
+        expect(record.style.size).toBeGreaterThan(0);
+        expect(record.style.sources.length).toBe(2);
+        done();
+      });
+    });
+
+    it("should record result of the component without style section (TestComponentWithoutStyle.vue)", (done) => {
+      webpack(webpackOption(plugin)).run((err, stats) => {
+        const record = recordFor("TestComponentWithoutStyle.vue");
+        expect(record.template.size).toBeGreaterThan(0);
+        expect(record.template.source.length).toBeGreaterThan(0);
+        expect(record.script.size).toBeGreaterThan(0);
+        expect(record.script.source.length).toBeGreaterThan(0);
+
+        expect(record.style.size).toBe(0);
+        expect(record.style.sources.length).toBe(0);
+        done();
+      });
+    });
+
+    it("should record result of the component with multiple style sections (TestComponentWithMultipleStyles.vue)", (done) => {
+      webpack(webpackOption(plugin)).run((err, stats) => {
+        const record = recordFor("TestComponentWithMultipleStyles.vue");
+        expect(record.template.size).toBeGreaterThan(0);
+        expect(record.template.source.length).toBeGreaterThan(0);
+        expect(record.script.size).toBeGreaterThan(0);
+        expect(record.script.source.length).toBeGreaterThan(0);
+        expect(record.style.size).toBeGreaterThan(0);
+
+        expect(record.style.sources.length).toBe(4);
+        done();
+      });
+    });
+  });
+
+  describe("stats file", () => {
+    it("should dump as default", (done) => {
+      const plugin = new Plugin();
+
+      expect(fileExists(defaultStatsPath)).toBeFalsy();
+      webpack(webpackOption(plugin)).run((err, stats) => {
+        expect(fileExists(defaultStatsPath)).toBeTruthy();
+        done();
+      });
+    });
+
+    it("should dump stats as JSON", (done) => {
+      const plugin = new Plugin();
+
+      webpack(webpackOption(plugin)).run((err, stats) => {
+        const content = fs.readFileSync(defaultStatsPath, "utf8");
+        expect(plugin.records).toEqual(JSON.parse(content));
+        done();
+      });
+    });
+
+    it("should dump with given filepath", (done) => {
+      const plugin = new Plugin({
+        statsFilename: customPath
+      });
+
+      expect(fileExists(customPath)).toBeFalsy();
+      webpack(webpackOption(plugin)).run((err, stats) => {
+        expect(fileExists(customPath)).toBeTruthy();
+        done();
+      });
+    });
+
+    it("should not dump if given false", (done) => {
+      const plugin = new Plugin({
+        statsFilename: false
+      });
+
+      expect(fileExists(defaultStatsPath)).toBeFalsy();
+      webpack(webpackOption(plugin)).run((err, stats) => {
+        expect(fileExists(defaultStatsPath)).toBeFalsy();
+        done();
+      });
     });
   });
 });
--- test/fixtures/source.ts
+++ test/fixtures/source.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import TestComponent from "./TestComponent.vue";
+import TestComponentWithoutStyle from "./TestComponentWithoutStyle.vue";
+import TestComponentWithMultipleStyles from "./TestComponentWithMultipleStyles.vue";
 
 const app = new Vue({
-  render: createElement => createElement(TestComponent)
+  render: createElement => createElement(TestComponent),
+  components: { TestComponentWithoutStyle, TestComponentWithMultipleStyles }
 }).$mount("#mountPoint");
