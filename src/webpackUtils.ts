7102209dc3c2bd1bd1b8903d5cb90bfed339d357
Lee Lundrigan <lee@indiegogo.com>
2018-06-08 10:28:21 -0700

    refactored regex into named constants

--- src/webpackUtils.ts
+++ src/webpackUtils.ts
@@ -1,4 +1,17 @@
+// ***********************
+// * Libraries & Plugins *
+// ***********************
+
 import { VueSFCAnalyzerRecord } from "./VueSFCAnalyzerWebpackPlugin";
+
+// *************
+// * Constants *
+// *************
+
+const VUE_FILE_REGEX = /.*vue-loader-options!([\w\-_/]+\.vue)\?.*$/
+const VUE_FILE_TEMPLATE_REGEX = /.*vue-loader-options!([\w\-_/]+\.vue)\?vue&type=template.*$/
+const VUE_FILE_SCRIPT_REGEX = /.*vue-loader-options!([\w\-_/]+\.vue)\?vue&type=script.*$/
+const VUE_FILE_STYLE_REGEX = /.*vue-loader-options!([\w\-_/]+\.vue)\?vue&type=style.*$/
 
 export const sectionByPortableId = (module: any): keyof VueSFCAnalyzerRecord | void => {
   const portableId = module.request;
@@ -7,17 +20,17 @@ export const sectionByPortableId = (module: any): keyof VueSFCAnalyzerRecord | v
   }
   const filename = vueFilePathByPortableId(portableId);
   if (filename) {
-    if (portableId.match(/.*vue-loader-options!([\w\-_/]+\.vue)\?vue&type=template.*$/)) {
+    if (portableId.match(VUE_FILE_TEMPLATE_REGEX)) {
       return "template";
-    } else if (portableId.match(/.*vue-loader-options!([\w\-_/]+\.vue)\?vue&type=script.*$/)) {
+    } else if (portableId.match(VUE_FILE_SCRIPT_REGEX)) {
       return "script";
-    } else if (portableId.match(/.*vue-loader-options!([\w\-_/]+\.vue)\?vue&type=style.*$/)) {
+    } else if (portableId.match(VUE_FILE_STYLE_REGEX)) {
       return "style";
     }
   }
 }
 
 export const vueFilePathByPortableId = (portableId: string): string | null => {
-  const matched = portableId.match(/.*vue-loader-options!([\w\-_/]+\.vue)\?.*$/);
+  const matched = portableId.match(VUE_FILE_REGEX);
   return matched && matched[1];
 }
